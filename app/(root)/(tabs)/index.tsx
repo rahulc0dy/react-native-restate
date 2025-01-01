231c3f4b0e29ddc9441115f796dc9f5cce68bc10
rahulc0dy <rahulchakraborty337@gmail.com>
2025-01-01 21:53:15 +0530

    refactor(tabs): remove unused navigation links from index component

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -11,12 +11,6 @@ export default function Index() {
       }}
     >
       <Text className="font-bold my-10 font-rubik text-3xl ">Welcome to restate</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">
-        <Text>Property 1</Text>
-      </Link>
     </View>
   );
 }
